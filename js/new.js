a6ddd16b527c2ef032543a9d40d9a9828f8b7607
Андрей Бабаев < >
2016-03-30 11:49:42 +0300

    Бронирование тура

---
+++ js/new.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+
+
+    $(function () {
+        $('.popup-modal').magnificPopup({
+            type: 'inline',
+            preloader: false,
+            focus: '#username',
+            closeBtnInside: true
+        });
+    });
+
+});
